ffc565a2a985baa0d4f6fb10cf3c7eb3ae340782
N0E-P <pecci.noe@gmail.com>
2024-06-10 12:29:18 +0200

    language detection

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		// Detect the browser's preferred language
+		const browserLang = navigator.language || navigator.userLanguage;
+		const supportedLocales = ["en", "fr"];
+		let detectedLocale = "en"; // fallback to 'en-US' if the browser's language is not supported
+
+		if (supportedLocales.includes(browserLang)) {
+			detectedLocale = browserLang;
+		}
+
+		// If the user hits the root path, redirect based on the browser's language
+		if (router.pathname === "/") {
+			router.replace(`/${detectedLocale}`);
+		}
+	}, [router.pathname]);
+
+	return <Component {...pageProps} />;
 }
-
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,3 @@
 export default function Home() {
 	return <div></div>;
 }
-
-// Redirect to the french or the english version
-export const getServerSideProps = async ({ locale }) => {
-	return {
-		redirect: {
-			destination: locale === "fr" ? "/fr" : "/en",
-			permanent: false,
-		},
-	};
-};
